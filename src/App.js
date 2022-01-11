2b2d5134112ac23adcced53edfdbbf791815b62e
petertran98 <Petertran198@gmail.com>
2022-01-10 16:32:00 -0800

    Added basic component layout for app  and styles for footer and header

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header/Header';
+import Home from './components/Home/Home';
+import MovieDetail from './components/MovieDetail/MovieDetail';
+import PageNotFound from './components/PageNotFound/PageNotFound';
+import Footer from './components/Footer/Footer';
+import { Switch, Route } from 'react-router-dom';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <Header />
+            <div className='container'>
+                <Switch>
+                    <Route path='/' exact component={Home} />
+                    <Route path='/movie/:imdbID' component={MovieDetail} />
+                    {/* Any route that does not match one of our predefined route will be in this page */}
+                    <Route component={PageNotFound} />
+                </Switch>
+            </div>
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './header.css';
+import { Link } from 'react-router-dom';
+import user from '../../common/images/user.png';
+export default function Header() {
+    return (
+        <div className='header'>
+            <Link to='/' className='logo'>
+                <div>Movie Ratings</div>
+            </Link>
+            <img src={user} className='user-image' alt='user portfolio image' />
+        </div>
+    );
+}
